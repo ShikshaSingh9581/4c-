736caf7522ed99dae204b7b210edc28ca981f021
ShikshaSingh9581 <153091897+ShikshaSingh9581@users.noreply.github.com>
2023-12-16 11:57:38 +0530

    Add files via upload

---
+++ pattern 7.cpp
@@ -0,0 +1,13 @@
+
+#include<iostream>
+using namespace std;
+int main()
+{
+  int i ,number;
+  cout<<"enter a number which you want to print";
+  cin>>number;
+  for(i=0;i<=number;i++)
+  {
+      cout<<i<<endl;
+  }
+}
